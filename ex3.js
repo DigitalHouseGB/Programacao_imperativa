c11d3e96d9b55ed5b1589bc09faedac2bba34a2e
Gustavo <gustavoserafio@gmail.com>
2022-08-09 22:16:36 -0300

    Exercicio IMC Programacao Imperativa

---
+++ ex3.js
@@ -0,0 +1,77 @@
+console.log(1 + 1);
+
+console.log(1 - 1);
+
+var numero_1 = 10;
+
+var numero_2 = 10;
+
+console.log ((numero_1) + (numero_2));
+
+var numero_3 = 10;
+
+console.log (20 + (numero_3));
+
+console.log ("20" + (numero_3));
+
+console.log ("ola" + 23);
+
+console.log ("gustavo " + 22);
+
+
+const nome_1 = "José";
+
+let Idade_1 = 27;
+
+let Peso_1 = 83.5;
+
+let Altura_1 = 1.70; 
+
+let Plano_1 = "Tem";
+
+let Imc_José = ((Peso_1) / ((Altura_1) * (Altura_1)));
+
+console.log ("José da Silva tem 27 anos e seu indice de massa corporal é " + (Imc_José) + " kgs");
+
+
+const nome_2 = "Carlos";
+
+let Idade_2 = 28;
+
+let Peso_2 = 80.1;
+
+let Altura_2 = 1.76; 
+
+let Plano_2 = "Tem";
+
+let Imc_Carlos = ((Peso_2) / ((Altura_2) * (Altura_2)));
+
+console.log ("Carlos de Souza tem 28 anos e seu indice de massa corporal é " + (Imc_Carlos) + " kgs");
+
+const nome_3 = "Aline";
+
+let Idade_3 = 33;
+
+let Peso_3 = 63.7;
+
+let Altura_3 = 1.53; 
+
+let Plano_3 = "Não";
+
+let Imc_Aline = ((Peso_3) / ((Altura_3) * (Altura_3)));
+
+console.log ("Aline Ferreira tem 33 anos e seu indice de massa corporal é " + (Imc_Aline) + " kgs");
+
+const nome_4 = "Ana";
+
+let Idade_4 = 26;
+
+let Peso_4 = 55;
+
+let Altura_4 = 1.62; 
+
+let Plano_4 = "Sim";
+
+let Imc_Ana = ((Peso_4) / ((Altura_4) * (Altura_4)));
+
+console.log ("Ana Paula tem 26 anos e seu indice de massa corporal é " + (Imc_Ana) + " kgs");
