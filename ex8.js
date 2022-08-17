3dea15197d6a0270e2be5c2f1012f32b6e4e1859
Gustavo <gustavoserafio@gmail.com>
2022-08-16 23:01:44 -0300

    Exercicio_8_de_função_com_if_else

---
+++ ex8.js
@@ -0,0 +1,40 @@
+/* Exercicio 1 */
+
+
+function podeSubir(alturaPessoa,acompanhada){
+    if (alturaPessoa > 1.40 && alturaPessoa < 2.00){
+    return Boolean
+}else if (alturaPessoa < 1.40 && acompanhada){
+    return Boolean
+}else {
+    return Boolean
+}
+}
+podeSubir()
+
+/* ----------------------------------- */
+
+/* Exercicio 2 */
+
+function podeSubir(alturaPessoa,acompanhada){
+    if (alturaPessoa > 1.40 && alturaPessoa < 2.00){
+    console.log("Acesso autorizado")
+}else if (alturaPessoa < 1.40 && acompanhada){
+    console.log("Acesso autorizado com acompanhante")
+}else {
+    console.log("Acesso negado")
+}
+}
+
+podeSubir()
+
+/* ------------------------------------ */
+
+
+
+
+
+
+
+/* criar função */
+/* com dois parametros */
